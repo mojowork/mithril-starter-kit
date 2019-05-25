8193c2b403993d4f27d5668a641425f450c6c632
chaos <huangchsh@ersoft.io>
2019-05-25 20:22:22 +0800

    Scss => Less

--- NMP.md
+++ NMP.md
@@ -4,8 +4,42 @@ npm install -D webpack webpack-cli (webpack)
 
 npm install -D babel-loader @babel/core @babel/preset-env (babel)
 
-npm install -D style-loader css-loader postcss-loader autoprefixer  cssnano node-sass sass-loader  (scss/less)
+{
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+}
+
+npm install -D style-loader css-loader postcss-loader autoprefixer  cssnano less less-loader  (less)
+// postcss-loader 
+{
+    loader: "postcss-loader", 
+    options: {
+      ident: 'postcss',
+      plugins: (loader) => [
+        require('autoprefixer')(),
+        require('cssnano')()
+      ]
+    }
+}
+
+npm install -D url-loader image-loader (images)
 
 npm install -D html-webpack-plugin (plugins)
+
+```
+## Target Browsers
+```
+package.json
+
+"browserslist": [ 
+  "last 2 version",
+	"> 1%",
+]
 
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,66 @@
-const path = require('path');
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+
   entry: {
     app: './src/app.js'
   },
 
   output: {
-    filename: '[name]-[hash:7].js',
+    filename: 'app.[hash:7].js',
     path: path.resolve(__dirname, 'dist')
   },
 
   module: {
     rules: [
       {
-        test:/\.jsx?$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+       {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader"
+        },
+        {
+          loader: "css-loader", 
+          options: {
+            sourceMap: true
+          }
+        },
+        {
+          loader: "postcss-loader", 
+          options: {
+            ident: 'postcss',
+            plugins: (loader) => [
+              require('autoprefixer')(),
+              require('cssnano')()
+            ]
+          }
+        },
+        {
+          loader: "less-loader", 
+          options: {
+            sourceMap: true
+          }
+        }]
       },
       {
-        test:/\.(scss|sass)$/,
+        test: /\.(png|jpg|gif)$/,
         use: [
-          { loader: 'style-loader', options: { sourceMap: true } },
-          { loader: 'css-loader', options: { sourceMap: true } },
-          { loader: 'postcss-loader', options: { sourceMap: true } },
-          { loader: 'sass-loader', options: { sourceMap: true } }
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
         ]
       }
     ]
@@ -34,4 +70,4 @@ module.exports = {
       template: './index.html'
     })
   ]
-};
+}
